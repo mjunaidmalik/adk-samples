84be7c3971cc0ce8228c8a7cbb1253a968d3ee7c
Shahin Saadati <shahins@google.com>
2025-12-08 16:34:55 -0800

    Reformatted the short-movie-agents sample by ensuring black, isort, and flake8 pass

--- python/agents/short-movie-agents/app/server.py
+++ python/agents/short-movie-agents/app/server.py
@@ -15,15 +15,14 @@
 import os
 
 import google.auth
+from app.utils.gcs import create_bucket_if_not_exists
+from app.utils.tracing import CloudTraceLoggingSpanExporter
+from app.utils.typing import Feedback
 from fastapi import FastAPI
 from google.adk.cli.fast_api import get_fast_api_app
 from google.cloud import logging as google_cloud_logging
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider, export
-
-from app.utils.gcs import create_bucket_if_not_exists
-from app.utils.tracing import CloudTraceLoggingSpanExporter
-from app.utils.typing import Feedback
 
 _, project_id = google.auth.default()
 logging_client = google_cloud_logging.Client()
